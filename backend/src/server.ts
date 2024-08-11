74e2d29f0a06628c543469bf1c3d2e755febce36
esuba <esuba419@gmail.com>
2024-08-11 23:05:18 +0300

    api route added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import post from './api/posts'
+
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT
 
-app.get('/', (req,res) => {
-    res.send({msg:"hello it is working ..."});
-})
+app.use(express.json())
+app.use(express.urlencoded())
+
+
+app.use('/api',post)
 
 app.listen(PORT, () => console.log(`surver running at port ${PORT}`));
