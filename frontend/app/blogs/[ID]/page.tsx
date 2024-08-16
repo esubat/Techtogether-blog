590fb7a2bee81d07c46d514933b2b7ba3c5f9464
esuba <esuba419@gmail.com>
2024-08-16 13:27:52 +0300

    data fetched using tanstack

--- frontend/app/blogs/[ID]/page.tsx
+++ frontend/app/blogs/[ID]/page.tsx
@@ -1,42 +1,32 @@
-'use client'
+"use client"
 
 import { useParams } from "next/navigation";
-import { useEffect, useState } from 'react';
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import Image from "next/image";
-import { BlogPostType } from "@/types/blogPost";
 import BlogDetail from "@/components/IndividualPost";
 import Loading from "@/components/animations/loading";
+import api from "@/utils/api/base";
+import LoadingError from "@/components/ErrorPage";
 
 export default function Component() {
     const params = useParams();
     const id = params?.ID;
-    const [blogPost, setBlogPost] = useState<BlogPostType | null>(null);
-
-    console.log(id)
-    useEffect(() => {
-
-        fetch(`http://localhost:8080/api/posts/${id}`)
-        .then((res)=>res.json())
-        .then(data => {
-          setBlogPost(data?.post)
-        })
-        .catch(error => {
-          console.log("can't fetch the blog!", error)
-        });
-    }, []);
-
-    if (!blogPost){
+    const {data, isLoading,isError} = api.blog.blogDetail.useQuery(id);
+    
+    if (isLoading){
       return(
         <Loading />
   )};
 
-console.log(blogPost)
+  if(isError){
+    return(
+      <LoadingError />
+    )
+  }
 
-return (
+
+  return (
     <div className="px-4 py-6 md:px-6 md:py-12 lg:py-16">
       <BlogDetail 
-       post={blogPost}
+       post={data}
       />
 
     </div>
--- frontend/app/blogs/page.tsx
+++ frontend/app/blogs/page.tsx
@@ -1,27 +1,34 @@
 'use client'
 
-import { useState, useEffect } from "react"
 import PostItems from "@/components/BlogList"
-import { BlogList } from "@/types/blogList"
+import api from "@/utils/api/base"
+import Loading from "@/components/animations/loading"
+import LoadingError from "@/components/ErrorPage";
 export default function BlogListPage() {
-  const [posts, setPosts] = useState<BlogList[]>([])
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/posts")
-      .then(res => res.json())
-      .then(data => {
-        setPosts(data?.posts || [])
-      })
-      .catch(error => {
-        console.error("Error fetching posts:", error)
-      })
-  }, [])
+  const {data, isLoading,isError} = api.blog.blogList.useQuery();
 
-  console.log(posts)
+  if (isLoading){
+    return(
+      <div className="flex justify-center">
+      <Loading />
+    </div>
+    )
+  }
+
+  if (isError){
+    return(
+      <div>
+        <LoadingError />
+      </div>
+    )
+  }
+
   return (
     <div className="flex justify-center">
       <div className="grid gap-6 max-w-3xl w-full px-4 py-12">
-        {posts.map((post) => {
+        {
+        data.map((post) => {
            return (
             <PostItems
               post={post}
