ef054dd3650f8e5646651040e1df70d5c5ee8fc0
esuba <esuba419@gmail.com>
2024-08-16 13:24:18 +0300

    tanstack added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/SiteHeader";
+import TanstackProvider from "@/provider/TanstackProvider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans", });
 
@@ -24,10 +25,12 @@ export default function RootLayout({
         inter.variable
       )} 
       >
+        <TanstackProvider>
       <div className="relative flex min-h-dvh flex-col bg-background">
         <SiteHeader />
            <main className="flex-1">{children}</main>
         </div>
+        </TanstackProvider>
      </body>
     </html>
   );
