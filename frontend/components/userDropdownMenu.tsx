f5e0feb4c68048d65ac27b768852796ce8c2a5c2
esuba <esuba419@gmail.com>
2024-08-16 13:21:02 +0300

    user dropdown menu

---
+++ frontend/components/userDropdownMenu.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image"
+import { 
+    DropdownMenu, 
+    DropdownMenuTrigger, 
+    DropdownMenuContent, 
+    DropdownMenuLabel, 
+    DropdownMenuSeparator, 
+    DropdownMenuItem } from "@/components/ui/dropdown-menu"
+    
+import {Button} from "@/components/ui/button"
+export default function UserDropdownMenu(){
+    return (
+        <div className="ml-auto flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
+                <Image
+                  src="/user-placeholder.png"
+                  width={36}
+                  height={36}
+                  alt="Avatar"
+                  className="overflow-hidden rounded-full"
+                  style={{ aspectRatio: "36/36", objectFit: "cover" }}
+                />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent >
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+    ) 
+}
